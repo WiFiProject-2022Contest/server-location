34d95880fc10cd15e4718f1f94fca43e567e13cf
xmxm00 <cmcm0012@gmail.com>
2022-06-14 16:49:05 +0900

    deleted useless files

--- src/main/java/com/wifiproject/demo/DemoApplication.java
+++ src/main/java/com/wifiproject/demo/DemoApplication.java
@@ -1,7 +1,13 @@
 package com.wifilocation.demo;
 
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+
+import java.util.logging.Logger;
 
 @SpringBootApplication
 public class DemoApplication {
--- src/main/java/com/wifiproject/demo/TestDAO.java
+++
@@ -1,8 +0,0 @@
-package com.wifilocation.demo;
-
-import com.wifilocation.demo.RSSID;
-import java.util.List;
-
-public interface TestDAO {
-    List<RSSID> getTestData();
-}
