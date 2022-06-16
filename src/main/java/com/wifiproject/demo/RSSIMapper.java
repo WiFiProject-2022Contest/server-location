41dafbc70a0a911c5ae021fede777d9acb23ac47
xmxm00 <cmcm0012@gmail.com>
2022-06-16 15:32:41 +0900

    fixxed bugs(result map for all query)

--- src/main/java/com/wifiproject/demo/RSSIMapper.java
+++ src/main/java/com/wifiproject/demo/RSSIMapper.java
@@ -14,6 +14,10 @@ public interface RSSIMapper {
     @Select("SELECT * FROM wifiinfo")
     List<RSSID> findAll();
 
+    @Results(id = "specificResultMap", value = {
+            @Result(property = "pos_x", column = "pos_x"),
+            @Result(property = "pos_y", column = "pos_y")
+    })
     @Select("SELECT * FROM wifiinfo WHERE pos_x >= #{pos_x} AND pos_x < #{pos_x} + 1 AND pos_y >= #{pos_y} AND pos_y < #{pos_y} + 1")
     List<RSSID> findPos(int pos_x, int pos_y);
 
