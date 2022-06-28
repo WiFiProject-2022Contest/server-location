cc0075de499b4249737723bf88b6dbce48f7f58b
백송현 <nanggu_pine@baegsonghyeon-ui-MacBookAir.local>
2022-06-28 13:21:58 +0900

    Database and API modified related to wifiinfo table

--- src/main/java/com/wifiproject/demo/RSSID.java
+++ src/main/java/com/wifiproject/demo/RSSID.java
@@ -18,10 +18,12 @@ public class RSSID {
     private String building;
     private Date date;
     private String uuid;
+    private String method;
 
+    public RSSID() {
+    }
 
-
-    public RSSID(int id, float pos_x, float pos_y, String SSID, String BSSID, int frequency, int level, String building, Date date, String uuid){
+    public RSSID(int id, float pos_x, float pos_y, String SSID, String BSSID, int frequency, int level, String building, Date date, String uuid, String method) {
         this.id = id;
         this.pos_x = pos_x;
         this.pos_y = pos_y;
@@ -32,9 +34,10 @@ public class RSSID {
         this.building = building;
         this.date = date;
         this.uuid = uuid;
+        this.method = method;
     }
-    public RSSID(){}
-    public RSSID(float pos_x, float pos_y, String SSID, String BSSID, int frequency, int level, String building, String uuid){
+
+    public RSSID(float pos_x, float pos_y, String SSID, String BSSID, int frequency, int level, String building, String uuid, String method) {
         this.pos_x = pos_x;
         this.pos_y = pos_y;
         this.SSID = SSID;
@@ -43,81 +46,94 @@ public class RSSID {
         this.level = level;
         this.building = building;
         this.uuid = uuid;
+        this.method = method;
     }
 
     public int getId() {
         return id;
     }
 
-    public Date getDate() {
-        return date;
+    public void setId(int id) {
+        this.id = id;
     }
 
     public float getPos_x() {
         return pos_x;
     }
 
-    public float getPos_y() {
-        return pos_y;
-    }
-
-    public String getSSID() {
-        return SSID;
-    }
-
-    public String getBSSID() {
-        return BSSID;
-    }
-
-    public int getFrequency() {
-        return frequency;
-    }
-
-    public int getLevel() {
-        return level;
-    }
-    public String getBuilding() {
-        return building;
-    }
-
-    public String getUuid() {
-        return uuid;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public void setPos_x(float pos_x) {
         this.pos_x = pos_x;
+    }
+
+    public float getPos_y() {
+        return pos_y;
     }
 
     public void setPos_y(float pos_y) {
         this.pos_y = pos_y;
     }
 
+    public String getSSID() {
+        return SSID;
+    }
+
     public void setSSID(String SSID) {
         this.SSID = SSID;
+    }
+
+    public String getBSSID() {
+        return BSSID;
     }
 
     public void setBSSID(String BSSID) {
         this.BSSID = BSSID;
     }
 
+    public int getFrequency() {
+        return frequency;
+    }
+
     public void setFrequency(int frequency) {
         this.frequency = frequency;
+    }
+
+    public int getLevel() {
+        return level;
     }
 
     public void setLevel(int level) {
         this.level = level;
     }
 
-    public void setDate(Date date) {
-        this.date = date;
+    public String getBuilding() {
+        return building;
     }
+
     public void setBuilding(String building) {
         this.building = building;
     }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
     public void setUuid(String uuid) {
         this.uuid = uuid;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
     }
 }
--- src/main/java/com/wifiproject/demo/RSSIMapper.java
+++ src/main/java/com/wifiproject/demo/RSSIMapper.java
@@ -64,8 +64,8 @@ public interface RSSIMapper {
 
     // Date 범위와 building, SSID에 따른 검색
 
-    @Insert("INSERT wifiinfo (pos_x, pos_y, SSID, BSSID, frequency, level, building, uuid) " +
-            "VALUES (#{pos_x}, #{pos_y}, #{SSID}, #{BSSID}, #{frequency}, #{level}, #{building}, #{uuid})")
+    @Insert("INSERT wifiinfo (pos_x, pos_y, SSID, BSSID, frequency, level, building, uuid, method) " +
+            "VALUES (#{pos_x}, #{pos_y}, #{SSID}, #{BSSID}, #{frequency}, #{level}, #{building}, #{uuid}, #{method})")
     void insert(RSSID rssid);
     void deleteDynamic(String building, String SSID, String uuid, Date from, Date to);
 
